8b7a6af3b848b9fbf6ff64b6a93039c9d446f2f6
Aditya Chakraborty <MIGHTYNEURONS.TECH@GMAIL.COM>
2022-07-26 18:37:36 +0530

    database

--- LmsApi/settings.py
+++ LmsApi/settings.py
@@ -132,8 +132,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_ffrom_env= dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_ffrom_env)
+db_from_env= dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
